061aab96976e8b57b0e6b03dab9d8050a50d823e
shaulab <shaul@datascience.co.il>
2024-11-03 15:35:08 +0200

    :wrench: :construction: configurations for research notebooks

---
+++ notebooks/config.py
@@ -0,0 +1,276 @@
+feature_groups = {
+    "Demographic Information": [
+        "age_03", "age_12", "urban_03", "urban_12", "married_03", "married_12",
+        "n_mar_03", "n_mar_12", "ragender", "sgender_03", "sgender_12"
+    ],
+    "Socioeconomic Status": [
+        "edu_gru_03", "edu_gru_12", "employment_03", "employment_12", "rearnings_03",
+        "rearnings_12", "searnings_03", "searnings_12", "hincome_03", "hincome_12",
+        "hinc_business_03", "hinc_business_12", "hinc_rent_03", "hinc_rent_12",
+        "hinc_assets_03", "hinc_assets_12", "hinc_cap_03", "hinc_cap_12",
+        "rinc_pension_03", "rinc_pension_12", "sinc_pension_03", "sinc_pension_12"
+    ],
+    "Health and Physical Limitations": [
+        "glob_hlth_03", "glob_hlth_12", "adl_dress_03", "adl_dress_12", "adl_walk_03",
+        "adl_walk_12", "adl_bath_03", "adl_bath_12", "adl_eat_03", "adl_eat_12",
+        "adl_bed_03", "adl_bed_12", "adl_toilet_03", "adl_toilet_12", "n_adl_03",
+        "n_adl_12", "iadl_money_03", "iadl_money_12", "iadl_meds_03", "iadl_meds_12",
+        "iadl_shop_03", "iadl_shop_12", "iadl_meals_03", "iadl_meals_12", "n_iadl_03",
+        "n_iadl_12", "hypertension_03", "hypertension_12", "diabetes_03", "diabetes_12",
+        "resp_ill_03", "resp_ill_12", "arthritis_03", "arthritis_12", "hrt_attack_03",
+        "hrt_attack_12", "stroke_03", "stroke_12", "cancer_03", "cancer_12",
+        "n_illnesses_03", "n_illnesses_12", "bmi_03", "bmi_12"
+    ],
+    "Mental Health and Well-being": [
+        "depressed_03", "depressed_12", "hard_03", "hard_12", "restless_03",
+        "restless_12", "happy_03", "happy_12", "lonely_03", "lonely_12", "enjoy_03",
+        "enjoy_12", "sad_03", "sad_12", "tired_03", "tired_12", "energetic_03",
+        "energetic_12", "n_depr_03", "n_depr_12", "cesd_depressed_03", "cesd_depressed_12"
+    ],
+    "Lifestyle and Behavior": [
+        "exer_3xwk_03", "exer_3xwk_12", "alcohol_03", "alcohol_12", "tobacco_03",
+        "tobacco_12", "test_chol_03", "test_chol_12", "test_tuber_03", "test_tuber_12",
+        "test_diab_03", "test_diab_12", "test_pres_03", "test_pres_12", "hosp_03",
+        "hosp_12", "visit_med_03", "visit_med_12", "out_proc_03", "out_proc_12",
+        "visit_dental_03", "visit_dental_12"
+    ],
+    "Social and Family Dynamics": [
+        "n_living_child_03", "n_living_child_12", "migration_03", "migration_12",
+        "decis_famil_03", "decis_famil_12", "decis_personal_03", "decis_personal_12",
+        "care_adult_12", "care_child_12", "volunteer_12", "attends_class_12",
+        "attends_club_12", "reads_12", "games_12", "table_games_12", "comms_tel_comp_12",
+        "act_mant_12", "tv_12", "sewing_12", "satis_ideal_12", "satis_excel_12",
+        "satis_fine_12", "cosas_imp_12", "wouldnt_change_12", "memory_12", "rrelgimp_03",
+        "rrelgimp_12", "rrfcntx_m_12", "rsocact_m_12", "rrelgwk_12"
+    ],
+    "Health Insurance and Coverage": [
+        "imss_03", "imss_12", "issste_03", "issste_12", "pem_def_mar_03", "pem_def_mar_12",
+        "insur_private_03", "insur_private_12", "insur_other_03", "insur_other_12",
+        "seg_pop_12", "insured_03", "insured_12"
+    ],
+    "Migration and U.S. Experience": [
+        "a16a_12", "a21_12", "a22_12", "a33b_12", "a34_12"
+    ],
+    "Housing and Environment": [
+        "j11_12"
+    ],
+    "Parental Education": [
+        "rameduc_m", "rafeduc_m"
+    ],
+    "Employment and Job History": [
+        "rjob_hrswk_03", "rjob_hrswk_12", "rjlocc_m_03", "rjlocc_m_12", "rjob_end_03",
+        "rjob_end_12", "rjobend_reason_03", "rjobend_reason_12"
+    ],
+    "Identifier": [
+        "uid", "year"
+    ]
+}
+
+feature_descriptions = {
+    # Identifier
+    'uid': 'Unique identifier for each respondent',
+    'year': 'Year of the survey',
+
+    # Demographic Information
+    'age_03': 'Binned age group',
+    'age_12': 'Binned age group',
+    'urban_03': 'Locality size. Either 0. <100,000 (rural) or 1. 100,000+ (urban)',
+    'urban_12': 'Locality size. Either 0. <100,000 (rural) or 1. 100,000+ (urban)',
+    'married_03': 'Marital status',
+    'married_12': 'Marital status',
+    'n_mar_03': 'Number of marriages',
+    'n_mar_12': 'Number of marriages',
+    'ragender': 'Gender',
+    'sgender_03': "Spouse's gender",
+    'sgender_12': "Spouse's gender",
+
+    # Socioeconomic Status
+    'edu_gru_03': 'Binned education level',
+    'edu_gru_12': 'Binned education level',
+    'employment_03': 'Employment status',
+    'employment_12': 'Employment status',
+    'rearnings_03': 'Earnings from employment',
+    'rearnings_12': 'Earnings from employment',
+    'searnings_03': "Spouse's earnings from employment",
+    'searnings_12': "Spouse's earnings from employment",
+    'hincome_03': 'Household income',
+    'hincome_12': 'Household income',
+    'hinc_business_03': 'Household income from business',
+    'hinc_business_12': 'Household income from business',
+    'hinc_rent_03': 'Household income from rent',
+    'hinc_rent_12': 'Household income from rent',
+    'hinc_assets_03': 'Household income from financial assets',
+    'hinc_assets_12': 'Household income from financial assets',
+    'hinc_cap_03': 'Household capital income',
+    'hinc_cap_12': 'Household capital income',
+    'rinc_pension_03': 'Income from pensions',
+    'rinc_pension_12': 'Income from pensions',
+    'sinc_pension_03': "Spouse's income from pensions",
+    'sinc_pension_12': "Spouse's income from pensions",
+
+    # Health and Physical Limitations
+    'glob_hlth_03': 'Self-reported global health',
+    'glob_hlth_12': 'Self-reported global health',
+    'adl_dress_03': 'Has difficulty getting dressed',
+    'adl_dress_12': 'Has difficulty getting dressed',
+    'adl_walk_03': 'Has difficulty walking from one side of the room to the other',
+    'adl_walk_12': 'Has difficulty walking from one side of the room to the other',
+    'adl_bath_03': 'Has difficulty bathing themselves in a tub or shower',
+    'adl_bath_12': 'Has difficulty bathing themselves in a tub or shower',
+    'adl_eat_03': 'Has difficulty eating',
+    'adl_eat_12': 'Has difficulty eating',
+    'adl_bed_03': 'Has difficulty getting in and out of bed',
+    'adl_bed_12': 'Has difficulty getting in and out of bed',
+    'adl_toilet_03': 'Has difficulty using the toilet',
+    'adl_toilet_12': 'Has difficulty using the toilet',
+    'n_adl_03': 'Number of activities of daily living (ADL) limitations (0-5)',
+    'n_adl_12': 'Number of activities of daily living (ADL) limitations (0-5)',
+    'iadl_money_03': 'Has difficulty managing money',
+    'iadl_money_12': 'Has difficulty managing money',
+    'iadl_meds_03': 'Has difficulty taking medications',
+    'iadl_meds_12': 'Has difficulty taking medications',
+    'iadl_shop_03': 'Has difficulty shopping for groceries',
+    'iadl_shop_12': 'Has difficulty shopping for groceries',
+    'iadl_meals_03': 'Has difficulty preparing a hot meal',
+    'iadl_meals_12': 'Has difficulty preparing a hot meal',
+    'n_iadl_03': 'Number of instrumental activities of daily living (IADL) limitations (0-4)',
+    'n_iadl_12': 'Number of instrumental activities of daily living (IADL) limitations (0-4)',
+    'hypertension_03': 'Has been diagnosed with hypertension',
+    'hypertension_12': 'Has been diagnosed with hypertension',
+    'diabetes_03': 'Has been diagnosed with diabetes',
+    'diabetes_12': 'Has been diagnosed with diabetes',
+    'resp_ill_03': 'Has been diagnosed with respiratory illness',
+    'resp_ill_12': 'Has been diagnosed with respiratory illness',
+    'arthritis_03': 'Has been diagnosed with arthritis/rheumatism',
+    'arthritis_12': 'Has been diagnosed with arthritis/rheumatism',
+    'hrt_attack_03': 'Has been told they had a heart attack',
+    'hrt_attack_12': 'Has been told they had a heart attack',
+    'stroke_03': 'Has been told they had a stroke',
+    'stroke_12': 'Has been told they had a stroke',
+    'cancer_03': 'Has been diagnosed with cancer',
+    'cancer_12': 'Has been diagnosed with cancer',
+    'n_illnesses_03': 'Number of illnesses (0-7)',
+    'n_illnesses_12': 'Number of illnesses (0-7)',
+    'bmi_03': 'Binned body mass index',
+    'bmi_12': 'Binned body mass index',
+
+    # Mental Health and Well-being
+    'depressed_03': 'Most of the past week, felt depressed',
+    'depressed_12': 'Most of the past week, felt depressed',
+    'hard_03': 'Most of the past week, felt that everything was an effort',
+    'hard_12': 'Most of the past week, felt that everything was an effort',
+    'restless_03': 'Most of the past week, felt that their sleep was restless',
+    'restless_12': 'Most of the past week, felt that their sleep was restless',
+    'happy_03': 'Most of the past week, felt happy',
+    'happy_12': 'Most of the past week, felt happy',
+    'lonely_03': 'Most of the past week, felt lonely',
+    'lonely_12': 'Most of the past week, felt lonely',
+    'enjoy_03': 'Most of the past week, felt that they enjoyed life',
+    'enjoy_12': 'Most of the past week, felt that they enjoyed life',
+    'sad_03': 'Most of the past week, felt sad',
+    'sad_12': 'Most of the past week, felt sad',
+    'tired_03': 'Most of the past week, felt tired',
+    'tired_12': 'Most of the past week, felt tired',
+    'energetic_03': 'Most of the past week, felt they had a lot of energy',
+    'energetic_12': 'Most of the past week, felt they had a lot of energy',
+    'n_depr_03': 'Number of CES-D depressive symptoms (0-9)',
+    'n_depr_12': 'Number of CES-D depressive symptoms (0-9)',
+    'cesd_depressed_03': 'Has 5+ CES-D depressive symptoms',
+    'cesd_depressed_12': 'Has 5+ CES-D depressive symptoms',
+
+    # Lifestyle and Behavior
+    'exer_3xwk_03': 'Exercises 3+ times per week',
+    'exer_3xwk_12': 'Exercises 3+ times per week',
+    'alcohol_03': 'Currently drinks alcohol',
+    'alcohol_12': 'Currently drinks alcohol',
+    'tobacco_03': 'Currently smokes tobacco',
+    'tobacco_12': 'Currently smokes tobacco',
+    'test_chol_03': 'Has had a cholesterol blood test',
+    'test_chol_12': 'Has had a cholesterol blood test',
+    'test_tuber_03': 'Has been tested for tuberculosis',
+    'test_tuber_12': 'Has been tested for tuberculosis',
+    'test_diab_03': 'Has been tested for diabetes',
+    'test_diab_12': 'Has been tested for diabetes',
+    'test_pres_03': 'Has been tested for high blood pressure',
+    'test_pres_12': 'Has been tested for high blood pressure',
+    'hosp_03': 'Has been hospitalized at least one night in the last year',
+    'hosp_12': 'Has been hospitalized at least one night in the last year',
+    'visit_med_03': 'Has visited a doctor at least once in the last year',
+    'visit_med_12': 'Has visited a doctor at least once in the last year',
+    'out_proc_03': 'Has had at least one outpatient procedure in the last year',
+    'out_proc_12': 'Has had at least one outpatient procedure in the last year',
+    'visit_dental_03': 'Has visited a dentist at least once in the last year',
+    'visit_dental_12': 'Has visited a dentist at least once in the last year',
+
+    # Social and Family Dynamics
+    'n_living_child_03': 'Binned number of living children',
+    'n_living_child_12': 'Binned number of living children',
+    'migration_03': 'Has lived or worked in the U.S.',
+    'migration_12': 'Has lived or worked in the U.S.',
+    'decis_famil_03': 'Weight in family decisions',
+    'decis_famil_12': 'Weight in family decisions',
+    'decis_personal_03': 'Weight over personal decisions',
+    'decis_personal_12': 'Weight over personal decisions',
+    'care_adult_12': 'Uses time to look after a sick or disabled adult',
+    'care_child_12': 'Uses time to look after children under 12',
+    'volunteer_12': 'Uses time to volunteer for a non-profit',
+    'attends_class_12': 'Uses time to attend training course, lecture, or class',
+    'attends_club_12': 'Uses time to attend sports or social club',
+    'reads_12': 'Uses time to read books, magazines, newspapers',
+    'games_12': 'Uses time to do crosswords, jigsaw puzzles, number games',
+    'table_games_12': 'Uses time to play tabletop games. E.g., cards, dominoes, chess',
+    'comms_tel_comp_12': 'Uses time to talk on the phone or send message/use the web on a computer',
+    'act_mant_12': 'Uses time to maintain a house, do repairs, garden, etc.',
+    'tv_12': 'Uses time to watch television',
+    'sewing_12': 'Uses time to sew, embroider, knit, make crafts',
+    'satis_ideal_12': 'How much they agree with the statement that their life is close to ideal',
+    'satis_excel_12': 'How much they agree with the statement that life is excellent',
+    'satis_fine_12': 'How much they agree with the statement that they are satisfied with their life',
+    'cosas_imp_12': 'How much they agree with the statement that they have achieved the things in life that are important to them',
+    'wouldnt_change_12': 'How much they agree with the statement that they would change almost nothing about their life',
+    'memory_12': 'Self-reported memory',
+    'rrelgimp_03': 'Importance of religion',
+    'rrelgimp_12': 'Importance of religion',
+    'rrfcntx_m_12': 'How often they see friends and relatives',
+    'rsocact_m_12': 'How often they have social activities',
+    'rrelgwk_12': 'Participates in weekly religious services',
+
+    # Health Insurance and Coverage
+    'imss_03': 'Has health coverage with IMSS',
+    'imss_12': 'Has health coverage with IMSS',
+    'issste_03': 'Has health coverage with ISSSTE/ISSSTE Estatal',
+    'issste_12': 'Has health coverage with ISSSTE/ISSSTE Estatal',
+    'pem_def_mar_03': 'Has health coverage with PEMEX, Defensa, or Marina',
+    'pem_def_mar_12': 'Has health coverage with PEMEX, Defensa, or Marina',
+    'insur_private_03': 'Has health coverage with private health insurance',
+    'insur_private_12': 'Has health coverage with private health insurance',
+    'insur_other_03': 'Has health coverage with other health insurance',
+    'insur_other_12': 'Has health coverage with other health insurance',
+    'seg_pop_12': 'Has health coverage with Seguro Popular',
+    'insured_03': 'Has health insurance',
+    'insured_12': 'Has health insurance',
+
+    # Migration and U.S. Experience
+    'a16a_12': 'Year when respondent first left for the U.S., if they ever lived in the U.S.',
+    'a21_12': 'Total years lived or worked in the U.S.',
+    'a22_12': 'Main job type during longest stay in the U.S.',
+    'a33b_12': 'U.S. residency status',
+    'a34_12': 'Speaks English',
+
+    # Housing and Environment
+    'j11_12': 'Floor material of residence',
+
+    # Parental Education
+    'rameduc_m': "Mother's education level",
+    'rafeduc_m': "Father's education level",
+
+    # Employment and Job History
+    'rjob_hrswk_03': 'Hours per week that they worked at their main job',
+    'rjob_hrswk_12': 'Hours per week that they worked at their main job',
+    'rjlocc_m_03': 'Category of their longest occupation',
+    'rjlocc_m_12': 'Category of their longest occupation',
+    'rjob_end_03': 'Year that their last job ended',
+    'rjob_end_12': 'Year that their last job ended',
+    'rjobend_reason_03': 'Reason that their last job ended',
+    'rjobend_reason_12': 'Reason that their last job ended'
+}
